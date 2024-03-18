4e859b7a022baeb08465f251b0c41f06b91780b3
nrennie <nrennie35@gmail.com>
2024-03-18 15:39:34 +0000

    add demo plots

---
+++ plot_1.R
@@ -0,0 +1,100 @@
+# Load packages -----------------------------------------------------------
+
+library(tidyverse)
+library(showtext)
+library(ggtext)
+
+
+# Load data ---------------------------------------------------------------
+
+tuesdata <- tidytuesdayR::tt_load("2023-11-21")
+rladies_chapters <- tuesdata$rladies_chapters
+
+
+# Load fonts --------------------------------------------------------------
+
+font_add_google("Yanone Kaffeesatz", "yanone")
+showtext_auto()
+
+
+# Define colours and fonts-------------------------------------------------
+
+bg_col <- "grey98"
+text_col <- "grey5"
+highlight_col <- "#562457"
+
+title_font <- "yanone"
+
+
+# Define text -------------------------------------------------------------
+
+title <- "R-Ladies Chapter Events"
+st <- "For both in person and online events, Thursdays are the most common day 
+of the week to hold R-Ladies chapter events."
+cap <- "**Data**: Meetup via Fgazzelloni | **Graphic**: N. Rennie"
+
+
+# Data wrangling ----------------------------------------------------------
+
+plot_data <- rladies_chapters |>
+  select(date, location, year) |>
+  mutate(date = lubridate::wday(date, label = TRUE, week_start = 1)) |>
+  count(date, location, year) |>
+  mutate(location = case_when(
+    location == "inperson" ~ "In person",
+    location == "online" ~ "Online"
+  ))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot(data = plot_data) +
+  geom_point(
+    mapping = aes(x = date, y = year, size = n),
+    colour = highlight_col,
+    fill = alpha(highlight_col, 0.5),
+    pch = 21
+  ) +
+  facet_wrap(~location) +
+  scale_y_reverse(breaks = min(plot_data$year):max(plot_data$year)) +
+  labs(
+    title = title,
+    subtitle = st,
+    caption = cap
+  ) +
+  theme_minimal(base_size = 24) +
+  theme(
+    plot.margin = margin(5, 5, 5, 15),
+    plot.background = element_rect(fill = bg_col, colour = bg_col),
+    panel.background = element_rect(fill = bg_col, colour = bg_col),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.major.x = element_line(
+      colour = highlight_col,
+      linewidth = 0.3
+    ),
+    strip.text = element_text(
+      family = title_font,
+      size = rel(1.2)
+    ),
+    axis.title = element_blank(),
+    plot.title = element_textbox_simple(
+      colour = text_col,
+      size = rel(2),
+      margin = margin(b = 10, t = 10),
+      lineheight = 0.5,
+      family = title_font
+    ),
+    plot.subtitle = element_textbox_simple(
+      colour = text_col,
+      margin = margin(b = 10, t = 0),
+      lineheight = 0.5
+    ),
+    plot.caption = element_textbox_simple(
+      colour = text_col,
+      margin = margin(b = 10, t = 5),
+      lineheight = 0.5
+    ),
+    plot.title.position = "plot",
+    plot.caption.position = "plot"
+  )
